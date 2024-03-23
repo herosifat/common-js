d20883fcefb95a52d9bb725620f5a61139491cd7
Your Namesifathero <sifathero466@gmail.com>
2024-03-23 19:54:41 +0600

    passby-ja

---
+++ passBy.js
@@ -0,0 +1,22 @@
+let student1={name: 'anata', partner : 'borsha'}
+let student2={name: 'raj', partner : 'mim'}
+
+function movieMake(cpu1,cpu2){
+    cpu1.name ='khakib';
+    cpu2.partner='akina';
+}
+console.log(student1,student2)
+movieMake(student1,student2);
+console.log(student1,student2)
+
+// more
+
+let love1 ={name:'imon', partner :'radika'}
+let love2 ={name:'apple', partner :'beli'}
+function loveDose(lv1,lv2){
+    lv1.name ='mamun';
+    lv2.partner='hashi';
+}
+console.log(love1,love2);
+loveDose(love1,love2);
+console.log(love1,love2);
