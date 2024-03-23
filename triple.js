80fd4dbdd358f1844ee7701bcd3cd757dbef4a36
Your Namesifathero <sifathero466@gmail.com>
2024-03-23 16:36:44 +0600

    triple-js

---
+++ triple.js
@@ -0,0 +1,22 @@
+/**
+ * == equal shudu value check kore but === equal value and type of duitai check kore
+ */
+
+const first =2;
+const second ='2';
+if( first == second){
+    console.log('vale are same in number')
+}
+else{
+    console.log('value are the not same in te number')
+}
+
+// more
+const firt =1;
+const secnd =true;
+if( firt === secnd){
+    console.log('vale are same in number')
+}
+else{
+    console.log('value are the not same in te number')
+}
