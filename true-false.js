7bf42f2a4f9326e529a84066c74c4f1b7c49f0bd
Your Namesifathero <sifathero466@gmail.com>
2024-03-23 16:15:37 +0600

    common-js

---
+++ true-false.js
@@ -0,0 +1,25 @@
+/*
+truthy:
+1.true
+2.any number (+, - ) will be true.
+3.any string other then empty string ' '
+.empty object,object,empty array,array.
+
+
+falsy:
+1.false
+2. 0.
+3.empty string ('')
+4. undefined
+5.null
+*/
+
+let x = {
+
+};
+ if(x){
+    console.log('x is truthy point')
+ }
+else{
+    console.log('x is falsy point');
+}
